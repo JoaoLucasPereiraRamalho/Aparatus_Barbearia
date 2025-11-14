50174b4c7a74ee204e974296358ae97c0d39e62f
Joao-Lucas-Ramalho <joao.lucas@hotsales.com.br>
2025-11-14 00:09:41 -0300

    feat: add header and image components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
-import Header from "./components/header";
+import Header from "./_components/header";
+import SearchInput from "./_components/search-input";
+import banner from "../public/banner.png";
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <Header />
+      <div className="space-y-4 px-5">
+        <SearchInput />
+        <Image
+          src={banner}
+          alt="Agende agora!"
+          sizes="100vw"
+          className="w-full h-auto"
+        />
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
